3d059589f04f33da2d272c3c10fa706b98097f47
Daniel Axelsson <daniel.axelsson4@gmail.com>
2018-03-01 13:13:59 +0100

    Another commit

--- src/models/Bus.java
+++ src/models/Bus.java
@@ -7,36 +7,12 @@ public class Bus extends Vehicle {
 
 	public Bus(String registrationNumber, String color, int numberOfWheels, int numberOfSeats) {
 		super(registrationNumber);
-		this.color = setColor(color);
 		this.numberOfWheels = numberOfWheels;
-		this.vehicleType = VehicleType.CAR;
+		this.vehicleType = VehicleType.BUS;
 
 	}
 
 
-	protected String setColor(String color) {
-		if (color.equals("Black")) {
-			this.color = color;
-		}
-		else if (color.equals("White") ) {
-			this.color = color;
-		}
-		else if (color.equals("Green") ) {
-			this.color = color;
-		}
-		else if (color.equals("Red") ) {
-			this.color = color;
-		}
-		else if (color.equals("Yellow") ) {
-			this.color = color;
-		}
-		else {
-			throw new ColorNotFoundException();
-		}
 
-		public String getColor() throws ColorNotFoundException {
-			return this.color;
-		}
-
-	}
+}
 
