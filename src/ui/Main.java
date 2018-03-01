d692487d0cb9100f8733795ec7fb1bdd5873a54b
Daniel Axelsson <daniel.axelsson4@gmail.com>
2018-03-01 15:55:07 +0100

    Airplane and car class update

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,8 +3,9 @@ package ui;
 
 import javax.swing.JOptionPane;
 
-import models.Car;
+import models.Boat;
 import models.ColorNotFoundException;
+import models.Garage;
 
 public class Main {
 
@@ -14,16 +15,31 @@ public class Main {
 		String registrationNumber = null;
 		String color = null;
 		Integer numberOfWheels = null;
+		int cylinderVolume = 0;
+		int numberOfSeats = 0;
+		int lifeboats = 0;
+		int length = 0;
+		Garage garageCatalog = new Garage();
 
-		numberOfWheels = Integer.parseInt(JOptionPane.showInputDialog("Input Number Of Wheels..."));
+
+
+
+
+		numberOfWheels = Integer.parseInt(JOptionPane.showInputDialog("Input Number of wheels..."));
 		color = JOptionPane.showInputDialog("Input Color of the Car...");
 		registrationNumber = JOptionPane.showInputDialog("Input registration number of the car ...");
+		cylinderVolume = Integer.parseInt(JOptionPane.showInputDialog("Input volume of cylinder in litres"));
+		numberOfSeats = Integer.parseInt(JOptionPane.showInputDialog("Input Number of seats"));
+		lifeboats = Integer.parseInt(JOptionPane.showInputDialog("Input number of lifeboats"));
+		length = Integer.parseInt(JOptionPane.showInputDialog("Input length in metres"));
 
 
 		try
 		{
-			Car car = new Car(registrationNumber, color, numberOfWheels.intValue());
-			System.out.println(car.toString());
+			Boat boat = new Boat(registrationNumber, color, length, lifeboats);
+			garageCatalog.addVehicle(boat);
+
+			System.out.println(boat.toString());
 		}
 		catch(ColorNotFoundException e)
 		{
